70364fb8fa61134ba8d1da74718ee0796c4a7dd9
Isa-hi <lacasademiamigochanel@gmail.com>
2024-04-27 11:55:05 -0600

    Complete basic use flow

--- server/client.js
+++ server/client.js
@@ -4,6 +4,7 @@ import env from 'dotenv';
 import axios from 'axios';
 import { Server } from 'socket.io';
 import { io } from 'socket.io-client';
+import bodyParser from 'body-parser';
 
 env.config();
 const app = express();
@@ -14,6 +15,9 @@ const CHAT_API = 'http://localhost:4000/'
 const socket = io(CHAT_API); // Conectar al servidor de chat
 
 app.use(express.static('public'));
+app.use(bodyParser.urlencoded({ extended: true }));
+
+let global_username;
 
 // Each client gets its own socket
 socket.on('connect', () => { // Conectar al servidor de chat
@@ -24,8 +28,53 @@ socket.on('connect', () => { // Conectar al servidor de chat
 app.get('/', async (req, res) => {
    const result = await axios.get(`${CHAT_API}`);
    const messagesObj = result.data; // Contiene id, message, user_id
-   console.log(messagesObj);
-   res.render(process.cwd() + '/client/chat.ejs', { messages: messagesObj });
+   //console.log(messagesObj);
+   res.render(process.cwd() + '/client/chat.ejs', { messages: messagesObj, user_name: global_username });
+});
+
+app.get('/register', async (req, res) => {
+    res.render(process.cwd() + '/client/register.ejs');
+});
+
+app.post('/register', async (req, res) => {
+    const user_name = req.body.user_name;
+    const password = req.body.password;
+    const result = await axios.post(`${CHAT_API}register`, {
+        user_name: user_name,
+        password: password
+    }, {
+        headers: {
+            'Content-Type': 'application/x-www-form-urlencoded',
+        }
+    });
+    if(result.status == 200) {
+        res.redirect('/login');
+    } else {
+        res.redirect('/register');
+    }
+});
+
+app.get('/login', async (req, res) => {
+    res.render(process.cwd() + '/client/login.ejs');
+});
+
+app.post('/login', async (req, res) => {
+    const user_name = req.body.user_name;
+    const password = req.body.password;
+    const result = await axios.post(`${CHAT_API}login`, {
+        user_name: user_name,
+        password: password
+    }, {
+        headers: {
+            'Content-Type': 'application/x-www-form-urlencoded',
+        }
+    });
+    if(result.status == 200) {
+        global_username = user_name;
+        res.redirect('/');
+    } else {
+        res.redirect('/login');
+    }
 });
 
 app.listen(port, () => {
--- server/server.js
+++ server/server.js
@@ -5,6 +5,7 @@ import { createServer } from 'node:http';
 import env from 'dotenv';
 import pg from 'pg';
 import cors from 'cors';
+import bodyParser from 'body-parser';
 
 env.config();
 const app = express();
@@ -34,6 +35,7 @@ app.use(cors({ // Habilitar CORS para que cualquier cliente pueda conectarse
     origin: '*',
     credentials: false, // No se requieren cookies para la conexión
 }));
+app.use(bodyParser.urlencoded({ extended: true }));
 
 
 io.on('connection', async (socket) => { // Each client gets its own socket
@@ -70,16 +72,38 @@ app.get('/', async (req, res) => {
     res.send(data);
 });
 
-app.get('/login', (req, res) => {
-    // LOGIN con User y Password
+app.post('/register', async (req, res) => {
+    console.log("server side", req.body);
+    try {
+        await db.query('CREATE TABLE IF NOT EXISTS users (id SERIAL PRIMARY KEY, user_name VARCHAR(50) UNIQUE NOT NULL, password VARCHAR(50) NOT NULL)');
+
+        await db.query('INSERT INTO users (user_name, password) VALUES ($1, $2) RETURNING *', [req.body.user_name, req.body.password]);
+        console.log('Usuario insertado correctamente: ', req.body);
+    } catch (error) {
+        console.error('Error al insertar el usuario', error);
+        return;
+    }
+    res.sendStatus(200);
 });
 
-app.get('/register', (req, res) => {
-    // REGISTER con User, Password
-});
-
-app.get('/chat', (req, res) => {
-    // CHAT tendrá identificación para saber qué usuario está accediendo
+app.post('/login', async (req, res) => {
+    console.log("server side", req.body);
+    let result;
+    try {
+        try {
+            const result = await db.query('SELECT * FROM users WHERE user_name = $1', [req.body.user_name]);
+            console.log('Usuario encontrado: ', result.rows[0]);
+        } catch (error) {
+            console.error('Usuario no existe', error);
+            res.sendStatus(500);
+        }
+        result = await db.query('SELECT * FROM users WHERE user_name = $1 AND password = $2', [req.body.user_name, req.body.password]);
+        console.log('Usuario encontrado: ', result.rows[0]);
+        res.sendStatus(200);
+    } catch (error) {
+        console.error('Contraseña incorrecta', error);
+        res.sendStatus(500);
+    }
 });
 
 server.listen(port, () => { // Escuchamos el servidor de HTTP 
